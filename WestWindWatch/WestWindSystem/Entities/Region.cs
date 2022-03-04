9cc422dead24c99167123065cf490d1ca1da3a30
ptyukasz <petertyukasz@gmail.com>
2022-03-04 11:07:28 -0700

    Region/BuildVersion Setup

---
+++ WestWindWatch/WestWindSystem/Entities/Region.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations; //for Key, Required, StringLength
+using System.ComponentModel.DataAnnotations.Schema; //for Table
+namespace WestWindSystem.Entities
+{   [Table("Regions")]
+    internal class Region
+    {   [Key]
+       public int RegionId { get; set; }
+       [Required(ErrorMessage = "Region Description is required")]
+       [StringLength(50, ErrorMessage = "Region Description is limited to 50 characters")]
+       public string RegionDescription { get; set; }
+    }
+}
